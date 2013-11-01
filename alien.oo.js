8e60393eb26a7b5356fd21db0537e970844567d8
janily <nanhuchenjie@163.com>
2013-11-01 19:23:29 +0800

    update
    update

---
+++ alien.oo.js
@@ -0,0 +1,49 @@
+create: function() {
+      var methods = null,
+          parent  = undefined,
+          klass   = function() {
+            this.$super = function(method, args) { return oo.$super(this.$parent, this, method, args); };
+            this.initialize.apply(this, arguments);
+          };
+
+      if (typeof arguments[0] === 'function') {
+        parent = arguments[0];
+        methods = arguments[1];
+      } else {
+        methods = arguments[0];
+      }
+
+      if (typeof parent !== 'undefined') {
+        oo.extend(klass.prototype, parent.prototype);
+        klass.prototype.$parent = parent.prototype;
+      }
+
+      oo.mixin(klass, methods);
+      oo.extend(klass.prototype, methods);
+      klass.prototype.constructor = klass;
+
+      if (!klass.prototype.initialize)
+        klass.prototype.initialize = function(){};
+
+      return klass;
+}
+mixin: function(klass, methods) {
+    if (typeof methods.include !== 'undefined') {
+      if (typeof methods.include === 'function') {
+        oo.extend(klass.prototype, methods.include.prototype);
+      } else {
+        for (var i = 0; i < methods.include.length; i++) {
+          oo.extend(klass.prototype, methods.include[i].prototype);
+        }
+      }
+    }
+  },
+
+extend: function(destination, source) {
+  for (var property in source)
+    destination[property] = source[property];
+  return destination;
+},
+$super: function(parentClass, instance, method, args) {
+  return parentClass[method].apply(instance, args);
+}
